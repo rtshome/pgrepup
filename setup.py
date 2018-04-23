1b5cc9cbf2eb234bdf217b7a1b056c3daa73c394
Denis Gasparin <rtshome@gmail.com>
2018-04-23 21:05:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 """Upgrade PostgreSQL clusters using logical replication
 
 pgrepup is an open-source tool for performing a major version upgrade between
-two PostgreSQL clusters using pglogical extension by 2nd Quadrant. PostgreSQL versions supported are 9.4, 9.5 and 9.6.
+two PostgreSQL clusters using pglogical extension by 2nd Quadrant. PostgreSQL versions supported are 9.4, 9.5, 9.6 and 10.
 pgrepup is distributed under GNU GPL 3 and maintained by Denis Gasparin <denis@gasparin.net>.
 """
 
